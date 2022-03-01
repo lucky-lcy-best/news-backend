e51c6bf9c994a07de4e8422862fab815b88fdfea
Best549 <2214187440@qq.com>
2022-03-01 19:01:35 +0800

    first version

---
+++ src/main/java/com/hfut/newsbackend/NewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hfut.newsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hfut/newsbackend/NewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hfut.newsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
